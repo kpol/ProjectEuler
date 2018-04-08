b261d471cc6530abf0e5408976377be921ebf1b3
Kirill Polishchuk <kirpolis@gmail.com>
2018-04-09 11:40:32 +1200

    Problem 35

---
+++ ProjectEuler/Common/Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Common
+{
+    public static class Number
+    {
+        public static ulong RotateInteger(ulong number)
+        {
+            var digits = (ulong)Math.Log10(number);
+            var factor = (ulong)Math.Pow(10, digits);
+
+            return number / 10 + factor * (number % 10);
+        }
+
+        public static IEnumerable<ulong> GetAllRotations(ulong number, bool includeOriginal = true)
+        {
+            if (includeOriginal)
+            {
+                yield return number;
+            }
+
+            var n = RotateInteger(number);
+
+            while (n != number)
+            {
+                yield return n;
+
+                n = RotateInteger(n);
+            }
+        }
+    }
+}
