c8f2cb5d23ce05e26e37fbb3a2dbb6d6bc48cdb2
Kirill Polishchuk <kirpolis@gmail.com>
2018-04-16 10:27:34 +1200

    -

--- ProjectEuler/Common/Prime.cs
+++ ProjectEuler/Common/Prime.cs
@@ -18,23 +18,24 @@ namespace ProjectEuler.Common
             {
                 yield return 2;
 
-                n /= 2;
-            }
-
-            while (n % 2 == 0)
-            {
-                n /= 2;
+                while (n % 2 == 0)
+                {
+                    n /= 2;
+                }
             }
 
             // n must be odd at this point. So we can
             // skip one element (Note i = i +2)
             for (ulong i = 3; i <= Math.Sqrt(n); i += 2)
             {
-                // While i divides n, print i and divide n
-                while (n % i == 0)
+                if (n % i == 0)
                 {
                     yield return i;
-                    n /= i;
+
+                    while (n % i == 0)
+                    {
+                        n /= i;
+                    }
                 }
             }
 
